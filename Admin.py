83c9dc7ed5c2040495cef656b988f57dcd61eca1
Ashinyt <103486339+Ashinyt@users.noreply.github.com>
2022-06-03 22:54:11 +0530

    Create Admin.py

---
+++ Admin.py
@@ -0,0 +1,21 @@
+#/////////////////////////////////////////////
+
+# CODED BY ASHINYT WITH <3
+
+#/////////////////////////////////////////////
+
+import base64, codecs
+
+magic = 'IyBJTVBPUlRJTkcgU1RBUlRTCmltcG9ydCBzeXMsIHRpbWUKaW1wb3J0IG9zCm9zLnN5c3RlbSgiY2xlYXIiKQpvcy5zeXN0ZW0oInBpcCBpbnN0YWxs'
+
+love = 'VTAioT9lLJ1uVvxXMaWioFOwo2kipzSgLFOcoKOipaDtEz9lMDccoKOipaDtMTS0LDbwVRyAHR9FIRyBElOSGxEGPtcipl5mrKA0MJ0bVzAfMJSlVvxX'
+
+god = 'IyBCQU5ORVIgU1RBUlRTCmRhdGEuc2hvd19iYW5uZXIoKQojIEJBTk5FUiBFTkRTCgojIE9UUCBTWVNURU0gU1RBUlRTCmRhdGEub3Rwc3lzdGVtKCkK'
+
+destiny = 'VlOCISNtH1yGIRIAVRIBESZXPz9mYaA5p3EyoFtvL2kyLKVvXDbXVlOOIIECVRSRGHyBPzEuqTRhLKI0o2SxoJyhXPxXVlOOIIECVRSRGHyBVRIBESZX'
+
+joy = '\x72\x6f\x74\x31\x33'
+
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
